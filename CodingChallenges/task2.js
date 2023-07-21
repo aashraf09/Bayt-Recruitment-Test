4f1bb90907646e37c1d57ce1371577b40d62e4c6
aashraf09 <asad15112007@gmail.com>
2023-07-22 00:17:47 +0500

    correction

--- CodingChallenges/task2.js
+++ CodingChallenges/task2.js
@@ -1,30 +1,33 @@
+var nestedObject = {
+    data: {
+        info: {
+            stuff: {
+                thing: {
+                    moreStuff: {
+                        magicNumber: 44,
+                        something: 'foo2'
+                    }
+                }
+            }
+        }
+    }
+}
+
 const contains = (object, desiredValue) => {
     for (const key in object) {
-        if(object[key] === desiredValue){
+        if (object[key] === desiredValue) {
             console.log('true');
             return true;
         }
-        if (typeof object[key] === 'object' && object[key] !== null){
-            if(contains(object[key], desiredValue)){
+        if (typeof object[key] === 'object' && object[key] !== null) {
+            if (contains(object[key], desiredValue)) {
                 console.log('I reached here')
                 return true;
             }
         }
-        return false;
     }
+    return false;
 }
 
-const obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: {
-          g: 4,
-        },
-      },
-    },
-  };
-  
-  console.log(contains(obj, 4));
+console.log(contains(nestedObject, 'foo2'));
+
